ce14140d07a8a2aa7f73305de55895351150a623
Wpp <1728211340@qq,com>
2022-02-11 01:39:43 +0800

    更新。

--- src/App.vue
+++ src/App.vue
@@ -23,16 +23,6 @@ export default {
   },
   mounted () {
     this.initUserSongList()
-    const app = document.getElementById('app')
-    const handleImgAndMessageClick = () => {
-      this.$store.commit('setAsyncShareImag', false) // 隐藏朋友圈图片
-      this.$refs.tabBar._data.isPrivate = false // 隐藏消息通知
-      this.$refs.tabBar._data.isisHistoryNewsPrivate = false
-      this.$bus.$emit('handleMenuMouseLeave') // 收起左侧路由导航栏
-    }
-    const stopMusic = e => e.keyCode === 32 && this.$refs.play_music.toggle()
-    window.addEventListener('keyup', stopMusic) // 监听键盘输入事件，空格停止音乐播放
-    app.addEventListener('click', handleImgAndMessageClick)
   }
 }
 </script>
--- src/layout/layout.vue
+++ src/layout/layout.vue
@@ -18,6 +18,18 @@ export default {
     CenterContent,
     PlayMusic
     // HomePageRecommends
+  },
+  mounted () {
+    const app = document.getElementById('app')
+    const handleImgAndMessageClick = () => {
+      this.$store.commit('setAsyncShareImag', false) // 隐藏朋友圈图片
+      this.$refs.tabBar._data.isPrivate = false // 隐藏消息通知
+      this.$refs.tabBar._data.isisHistoryNewsPrivate = false
+      this.$bus.$emit('handleMenuMouseLeave') // 收起左侧路由导航栏
+    }
+    const stopMusic = e => e.keyCode === 32 && this.$refs.play_music.toggle()
+    window.addEventListener('keyup', stopMusic) // 监听键盘输入事件，空格停止音乐播放
+    app.addEventListener('click', handleImgAndMessageClick)
   }
 }
 </script>
