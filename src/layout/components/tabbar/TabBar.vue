4c2b59077fbf07702dd14b5524ca97591f0c0c25
Wpp <1728211340@qq,com>
2022-05-23 14:23:00 +0800

    更新。

--- src/layout/components/tabbar/TabBar.vue
+++ src/layout/components/tabbar/TabBar.vue
@@ -8,7 +8,7 @@
       <button class="el-icon-arrow-left" @click.stop="handleChangeRouter(-1)" />
       <button class="el-icon-arrow-right" @click.stop="handleChangeRouter(1)" />
     </div>
-    <music-search />
+    <musicSearch />
     <div class="user-group">
       <div class="img-container">
         <img :src="getUserImage" @click="showLogin()" />
@@ -17,9 +17,7 @@
       <div v-if="isLogin">
         <el-dropdown @command="handleDropClick" @visible-change="handleDropMenuVisible">
           <span class="el-dropdown-link">
-            <div class="droupdown-container">
-              设置<i :class="[dropVisiable? 'el-icon-arrow-up': 'el-icon-arrow-down', 'el-icon--right']" />
-            </div>
+            <div class="droupdown-container">设置<i :class="[dropVisiable ? 'el-icon-arrow-up' : 'el-icon-arrow-down', 'el-icon--right']" /></div>
           </span>
           <el-dropdown-menu slot="dropdown">
             <el-dropdown-item v-for="(item, index) in droupMenuList" :key="index" :command="item.value">
@@ -28,8 +26,7 @@
                   <i class="el-icon-picture-outline-round" />{{ item.label }}
                 </el-upload>
               </label>
-              <label v-else class="droup-label">
-                <i :class="item.icon" /> {{ item.label }}</label>
+              <label v-else class="droup-label"> <i :class="item.icon" /> {{ item.label }}</label>
             </el-dropdown-item>
           </el-dropdown-menu>
         </el-dropdown>
@@ -49,8 +46,14 @@
     <!-- 消息通知(私信，评论，@我，通知) -->
     <messageNotice v-if="isPrivate" class="private_detail" @privateNewChange="privateNewChange" />
     <!-- 消息通知的详情页面 -->
-    <privateMessageDetail v-if="isHistoryNews" class="private_detail" :history-list="historyList" :more="historyMore" @prePrivateDetail="prePrivateDetail"
-      @visiableMessage="(isPrivate = false), (isHistoryNews = false)" />
+    <privateMessageDetail
+      v-if="isHistoryNews"
+      class="private_detail"
+      :history-list="historyList"
+      :more="historyMore"
+      @prePrivateDetail="prePrivateDetail"
+      @visiableMessage="(isPrivate = false), (isHistoryNews = false)"
+    />
   </div>
 </template>
 <script>
@@ -58,38 +61,38 @@ import { mapState, mapGetters, mapActions } from 'vuex';
 import { _setUserImage } from '@/network/user';
 import { _getPrivateHistoryNews, HandlePrivateHistory } from '@/network/privateNews';
 import { mixins } from '@/assets/common/verify-phone';
-import MusicSearch from '@/components/content/search/MusicSearch';
+import musicSearch from '@/views/musicSearch/index';
 import messageNotice from '@/views/messageNotice/index';
 import privateMessageDetail from '@/views/privateMessageDetail/index';
 export default {
   name: 'TabBar',
-  components: { MusicSearch, privateMessageDetail, messageNotice },
+  components: { musicSearch, privateMessageDetail, messageNotice },
   mixins: [mixins],
   data() {
     return {
       header: {
-        'Content-Type': 'multipart/form-data',
+        'Content-Type': 'multipart/form-data'
       },
       droupMenuList: [
         {
           icon: 'el-icon-picture-outline-round',
           value: 'editImg',
-          label: '修改头像',
+          label: '修改头像'
         },
         { icon: 'el-icon-message', value: 'messageNotify', label: '消息通知' },
-        { icon: 'el-icon-s-home', value: 'layout', label: '退出登录' },
+        { icon: 'el-icon-s-home', value: 'layout', label: '退出登录' }
       ],
       isPrivate: false,
       isHistoryNews: false,
       historyList: [],
       privageUserId: '',
       historyMore: false,
-      dropVisiable: false,
+      dropVisiable: false
     };
   },
   computed: {
     ...mapState(['userName']),
-    ...mapGetters(['getUserImage', 'isLogin']),
+    ...mapGetters(['getUserImage', 'isLogin'])
   },
   watch: {
     // 隐藏消息和私信内容
@@ -98,8 +101,8 @@ export default {
         this.isPrivate = false;
         this.isHistoryNews = false;
       },
-      deep: true,
-    },
+      deep: true
+    }
   },
   methods: {
     ...mapActions(['_GETUSERINFO']),
@@ -141,8 +144,8 @@ export default {
       uploadImage.append('imgFile', item.file);
       const {
         data: {
-          data: { code },
-        },
+          data: { code }
+        }
       } = await _setUserImage(uploadImage);
       if (code === 200) {
         this.$message.success('头像修改成功');
@@ -165,153 +168,153 @@ export default {
     prePrivateDetail() {
       this.isPrivate = true;
       this.isHistoryNews = false;
-    },
-  },
+    }
+  }
 };
 </script>
 <style lang="less" scoped>
-  .header {
-    width: 100%;
-    height: 54px;
-    background-color: #f5f5f7;
-    color: #0a0a0a;
-    user-select: none;
+.header {
+  width: 100%;
+  height: 54px;
+  background-color: #f5f5f7;
+  color: #0a0a0a;
+  user-select: none;
+}
+.logo {
+  position: relative;
+  display: flex;
+  float: left;
+  width: 25%;
+  height: 100%;
+  justify-content: flex-start;
+  align-items: center;
+  cursor: pointer;
+  z-index: 0.8;
+  > img {
+    width: 36px;
+    height: 36px;
+    margin-left: 10px;
+    z-index: 10000;
   }
-  .logo {
-    position: relative;
+  > img:hover {
+    transform: translate(600px, 600px) scale(20) rotate(360deg);
+    transition: 3s all;
+  }
+  > .title {
+    display: inline-block;
+    padding-left: 8px;
+    width: 120px;
+    height: 20px;
+    font-size: 16px;
+    color: #0a0a0a;
+  }
+}
+
+.buttons {
+  position: relative;
+  float: left;
+  width: 10%;
+  height: 100%;
+  line-height: 54px;
+  z-index: 1;
+  > button {
+    display: inline-block;
+    width: 30%;
+    height: 26px;
+    border: 0.2px solid #f5f5f7;
+    color: #0a0a0a;
+    outline: none;
+    cursor: pointer;
+  }
+}
+
+.el-icon-arrow-left,
+.el-icon-arrow-right {
+  font-weight: 200;
+}
+
+.user-group {
+  display: flex;
+  width: 37%;
+  height: 100%;
+  justify-content: flex-end;
+  align-items: center;
+  .img-container {
+    width: 32px;
+    height: 32px;
+    border-radius: 50%;
+    & img {
+      width: 100%;
+      height: 100%;
+      border-radius: 50%;
+      cursor: pointer;
+    }
+    &:hover {
+      .info-container {
+        display: block;
+      }
+    }
+  }
+
+  & .droupdown-container {
+    font-size: 16px;
+    color: #0a0a0a;
+    cursor: pointer;
+  }
+
+  & > div {
+    margin-left: 10px;
+  }
+}
+
+.droup-label {
+  & i {
+    font-size: 18px !important;
+  }
+}
+
+.info-container {
+  display: none;
+  position: absolute;
+  width: 300px;
+  height: auto;
+  padding: 10px;
+  top: 54px;
+  right: 0px;
+  border: 1px solid black;
+  background: #fff;
+  border-radius: 10px;
+  box-shadow: 0 0 2px rgb(241, 236, 236);
+  z-index: 10000;
+
+  .user-info {
     display: flex;
-    float: left;
-    width: 25%;
-    height: 100%;
     justify-content: flex-start;
     align-items: center;
-    cursor: pointer;
-    z-index: 0.8;
-    > img {
-      width: 36px;
-      height: 36px;
-      margin-left: 10px;
-      z-index: 10000;
-    }
-    > img:hover {
-      transform: translate(600px, 600px) scale(20) rotate(360deg);
-      transition: 3s all;
-    }
-    > .title {
+    & img {
       display: inline-block;
-      padding-left: 8px;
-      width: 120px;
-      height: 20px;
-      font-size: 16px;
-      color: #0a0a0a;
-    }
-  }
-
-  .buttons {
-    position: relative;
-    float: left;
-    width: 10%;
-    height: 100%;
-    line-height: 54px;
-    z-index: 1;
-    > button {
-      display: inline-block;
-      width: 30%;
-      height: 26px;
-      border: 0.2px solid #f5f5f7;
-      color: #0a0a0a;
-      outline: none;
-      cursor: pointer;
-    }
-  }
-
-  .el-icon-arrow-left,
-  .el-icon-arrow-right {
-    font-weight: 200;
-  }
-
-  .user-group {
-    display: flex;
-    width: 37%;
-    height: 100%;
-    justify-content: flex-end;
-    align-items: center;
-    .img-container {
-      width: 32px;
-      height: 32px;
+      width: 60px !important;
+      height: 60px !important;
       border-radius: 50%;
-      & img {
-        width: 100%;
-        height: 100%;
-        border-radius: 50%;
-        cursor: pointer;
-      }
-      &:hover {
-        .info-container {
-          display: block;
-        }
-      }
     }
 
-    & .droupdown-container {
-      font-size: 16px;
-      color: #0a0a0a;
-      cursor: pointer;
-    }
-
-    & > div {
-      margin-left: 10px;
+    .user-name {
+      margin-left: 8px;
+      font-size: 20px;
     }
   }
+}
 
-  .droup-label {
-    & i {
-      font-size: 18px !important;
-    }
-  }
+.el-icon-picture-outline-round {
+  display: inline-block;
+  font-size: 30px !important;
+  vertical-align: -2px;
+}
 
-  .info-container {
-    display: none;
-    position: absolute;
-    width: 300px;
-    height: auto;
-    padding: 10px;
-    top: 54px;
-    right: 0px;
-    border: 1px solid black;
-    background: #fff;
-    border-radius: 10px;
-    box-shadow: 0 0 2px rgb(241, 236, 236);
-    z-index: 10000;
-
-    .user-info {
-      display: flex;
-      justify-content: flex-start;
-      align-items: center;
-      & img {
-        display: inline-block;
-        width: 60px !important;
-        height: 60px !important;
-        border-radius: 50%;
-      }
-
-      .user-name {
-        margin-left: 8px;
-        font-size: 20px;
-      }
-    }
-  }
-
-  .el-icon-picture-outline-round {
-    display: inline-block;
-    font-size: 30px !important;
-    vertical-align: -2px;
-  }
-
-  .private_detail {
-    position: absolute;
-    top: 54px;
-    right: 0;
-    z-index: 10000;
-  }
+.private_detail {
+  position: absolute;
+  top: 54px;
+  right: 0;
+  z-index: 10000;
+}
 </style>
