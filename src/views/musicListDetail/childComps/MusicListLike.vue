57323878d0893d745a23ef0f51b479ad8f1a60b3
Wpp <1728211340@qq,com>
2022-05-24 14:26:41 +0800

    更新。

--- src/views/musicListDetail/childComps/MusicListLike.vue
+++ src/views/musicListDetail/childComps/MusicListLike.vue
@@ -2,9 +2,9 @@
   <div class="music-like-container">
     <div class="musiclist-like" v-if="subs.length !== 0">
       <div class="content">
-        <div class="item" v-for="(item,index) in subs " :key="index">
-          <img src="" :data-src="item.avatarUrl + '?param=60y60'" alt="" v-imgLazy>
-          <div>{{item.nickname}}</div>
+        <div class="item" v-for="(item, index) in subs" :key="index">
+          <img src="" :data-src="item.avatarUrl + '?param=60y60'" alt="" v-imgLazy />
+          <div>{{ item.nickname }}</div>
         </div>
       </div>
     </div>
@@ -22,37 +22,37 @@ export default {
       default: () => []
     }
   }
-}
+};
 </script>
 <style lang="less" scoped>
-  .music-like-container {
+.music-like-container {
+  width: 100%;
+  margin-bottom: 20px;
+
+  & .musiclist-like {
     width: 100%;
-    margin-bottom: 20px;
+    > .content {
+      display: flex;
+      flex-wrap: wrap;
 
-    & .musiclist-like {
-      width: 100%;
-      > .content {
-        display: flex;
-        flex-wrap: wrap;
-
-        & .item {
-          width: 160px;
-          padding: 30px;
-          text-align: center;
-          font-size: 12px;
-          cursor: pointer;
-          > img {
-            width: 60px;
-            height: 60px;
-            border-radius: 50%;
-          }
+      & .item {
+        width: 160px;
+        padding: 30px;
+        text-align: center;
+        font-size: 12px;
+        cursor: pointer;
+        > img {
+          width: 60px;
+          height: 60px;
+          border-radius: 50%;
         }
       }
     }
-
-    & .none-music-collect {
-      padding: 10px 0px 10px 30px;
-      font-size: 14px;
-    }
   }
-</style>>
+
+  & .none-music-collect {
+    padding: 10px 0px 10px 30px;
+    font-size: 14px;
+  }
+}</style
+>>
